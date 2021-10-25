c6296a3cc3f9819e92ca37cb089f70cd2448ca42
Nicklas Hugoy <nicklas.hug@gmail.com>
2021-10-25 10:29:20 +0200

    implement matrix transform for assignment

--- src/transforms_matrix.h
+++ src/transforms_matrix.h
@@ -39,7 +39,7 @@ class MatrixTransforms : public CfgVisitor<LatticeType>
 
         void visit_initializtion(InitializerNode<LatticeType>& node){
             std::shared_ptr<int[]> matrix = unit_matrix();
-            std::set<std::string> expr_vars = node.expression->get_variables();;
+            std::set<std::string> expr_vars = node.expression->get_variables();
             int id_index = variables[node.id];
 
             matrix.get()[rowSize*id_index+id_index] = 0;
@@ -61,7 +61,23 @@ class MatrixTransforms : public CfgVisitor<LatticeType>
         void visit_assignReturn(AssignReturnNode<LatticeType>& node){
         }
 
-        void visit_assignment(AssignmentNode<LatticeType>& node){}
+        void visit_assignment(AssignmentNode<LatticeType>& node){
+            std::shared_ptr<int[]> matrix = unit_matrix();
+            std::set<std::string> expr_vars = node.expression->get_variables();
+            int id_index = variables[node.id];
+
+            matrix.get()[rowSize*id_index+id_index] = 0;
+
+            for(std::string expr_var : expr_vars){
+                if (variables.count(expr_var)){
+                    matrix.get()[rowSize*variables[expr_var]+id_index] = 1;
+                }
+            }     
+            
+
+            matrices.push_back(matrix);
+        }
+
         void visit_functioncall(FunctionCall<LatticeType>& node){}
         void visit_if(IfNode<LatticeType>& node){}
         void visit_return(ReturnNode<LatticeType>& node){}
