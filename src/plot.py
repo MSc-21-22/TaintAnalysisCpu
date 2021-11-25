821b3755ea59ff2e4748fceb3a579b25ec3f39df
Nicklas Hugoy <nicklas.hug@gmail.com>
2021-11-25 13:02:39 +0100

    bar plot of avereage values

--- src/plot.py
+++ src/plot.py
@@ -1,15 +1,18 @@
 import matplotlib
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
+import numpy
 
 matplotlib.use('TkAgg')
 
 values = genfromtxt("timings.csv", delimiter=',', usecols=(0,1,2))
-print(values)
+averages = numpy.average(values,axis=0)
 
 fig = plt.figure(figsize=(10,7))
 
-plt.boxplot(values, labels=['CPU', 'GPU cuBLAS','GPU cuBool'], widths=0.6)
+labels = ['CPU', 'GPU cuBLAS','GPU cuBool']
+# plt.boxplot(values, labels=['CPU', 'GPU cuBLAS','GPU cuBool'], widths=0.6)
+plt.bar(labels, averages)
 plt.yscale("log")
 
 plt.show()
