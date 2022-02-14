bc31cf99ace4b4be2cd6918404f84af8f421f4be
Thorulf Neustrup <thorulf@live.dk>
2022-02-14 11:33:59 +0100

    Added cuda draft implementation

---
+++ src/bit_cuda/analysis.cu
@@ -0,0 +1,54 @@
+struct Transfer{ // x = y + z     { x, [y, z]}
+    int x;
+    int rhs[5]; // -1 terminated
+};
+
+struct BitVector{
+    long int data;
+};
+
+struct Node{
+    Transfer transfer;
+    int predecessor_index[5];
+    BitVector data;
+};
+
+__global__ void analyze(Node nodes[], bool* has_changed){
+    int node_index = threadIdx.x + blockDim.x * blockIdx.x;
+
+    nodes[node_index].data.data = 1; // Set taint constant to true
+
+    while(*has_changed){
+        *has_changed = false;
+        bool is_changed = false;
+        long int new_data = 0;
+        //Join
+        {
+            long int old_data = nodes[node_index].data.data;
+            new_data = old_data;
+            int pred_index = 0;
+            while (nodes[node_index].predecessor_index[pred_index] != -1){
+                new_data |= nodes[nodes[node_index].predecessor_index[pred_index]].data.data;
+                ++pred_index;
+            }
+
+            is_changed = old_data == new_data;
+        }
+
+        //Transfer
+        if(is_changed){
+            int var_index = 0;
+            int next_var = nodes[node_index].transfer.rhs[var_index];
+            while(next_var != -1){
+                if(nodes[node_index].data.data & (1 << next_var) > 0){
+                    nodes[node_index].data.data |= nodes[node_index].transfer.x;
+                    break;
+                }
+                ++var_index;
+                next_var = nodes[node_index].transfer.rhs[var_index];
+            }
+
+            *has_changed = true;
+        }
+    }
+}
