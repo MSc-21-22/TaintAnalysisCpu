30d941c981205dfe88d47d6fb83a8d9fcbbd1c77
Thorulf Neustrup <thorulf@live.dk>
2022-03-25 13:18:45 +0100

    Updated worklist algorithm to include latest bugfixes

--- src/cuda/multi_cuda/analysis.cu
+++ src/cuda/multi_cuda/analysis.cu
@@ -8,6 +8,7 @@
 #include "analysis.h"
 
 #define THREAD_COUNT 1024
+#define EXTRA_WORKLISTS 50
 #define COLLISIONS_BEFORE_SWITCH (1)
 
 using namespace multi_cuda;
@@ -19,29 +20,33 @@ __device__ inline Node& get_node(Node* nodes, int index, int node_size){
     return *(Node*)(byte_ptr + byte_offset);
 }
 
-__device__ void add_sucessors_to_worklist(int* successors, int work_columns[][THREAD_COUNT], int work_column_count, int current_work_column){
+__device__ void add_sucessors_to_worklist(int* successors, int work_columns[][THREAD_COUNT], int work_column_count, int current_work_column, int* worklists_pending){
     int initial_work_column = current_work_column;
     for(int i = 0; i < 5; i++){
+        current_work_column = initial_work_column;
+        int amount_of_new_worklists = 1;
         int succ_index = successors[i];
         if (succ_index == -1)
             return;
         intptr_t hash = succ_index * 120811; 
         int collision_count = 0;
         int* work_column = work_columns[current_work_column];
-        while(work_column[hash % THREAD_COUNT] != -1){
+        while(atomicCAS(&work_column[hash % THREAD_COUNT], -1, succ_index) != -1){
             if(work_column[hash % THREAD_COUNT] == succ_index){
                 break;
             }
             
-
             if(++collision_count >= COLLISIONS_BEFORE_SWITCH){
                 current_work_column = (current_work_column + 1) % work_column_count;
                 work_column = work_columns[current_work_column];
+                amount_of_new_worklists++;
+                collision_count = 0;
             }else{
                 hash++;
             }
         }
-        work_column[hash % THREAD_COUNT] = succ_index;
+        
+        atomicMax(worklists_pending, amount_of_new_worklists);
     }
 }
 
@@ -55,13 +60,12 @@ __device__ BitVector multi_cuda_join(int predecessors[], Node *nodes, int node_s
         return joined_data;
 }
 
-__global__ void analyze(Node* nodes, int work_columns[][THREAD_COUNT], int work_column_count, Transfer transfers[], int node_count, bool* work_to_do, int i, int source_count){
+__global__ void analyze(Node* nodes, int work_columns[][THREAD_COUNT], int work_column_count, Transfer transfers[], int node_count, int* worklists_pending, int current_work_column, int source_count){
     int node_index = threadIdx.x + blockDim.x * blockIdx.x;
-    int* work_column = work_columns[i];
+    int* work_column = work_columns[current_work_column];
     int node_size = sizeof(Node) + sizeof(BitVector) * source_count;
 
     if(node_index < THREAD_COUNT && work_column[node_index] != -1){
-        int index = node_size * work_column[node_index];
         Node& current_node = get_node(nodes, work_column[node_index], node_size);
 
         bool add_successors = false;
@@ -84,8 +88,7 @@ __global__ void analyze(Node* nodes, int work_columns[][THREAD_COUNT], int work_
         }
 
         if(add_successors){
-            add_sucessors_to_worklist(current_node.successor_index, work_columns, work_column_count, (i+1) % work_column_count);
-            *work_to_do = true;
+            add_sucessors_to_worklist(current_node.successor_index, work_columns, work_column_count, (current_work_column+1) % work_column_count, worklists_pending);
         }
 
         work_column[node_index] = -1;   
@@ -94,14 +97,15 @@ __global__ void analyze(Node* nodes, int work_columns[][THREAD_COUNT], int work_
 
 void multi_cuda::execute_analysis(Node* nodes, int node_count, Transfer* transfers, int transfer_count, std::set<int>& taint_sources, int source_count) {
     Node* dev_nodes = nullptr;
-    bool* dev_work_to_do = nullptr;
     Transfer* dev_transfers = nullptr;
     int** dev_worklists = nullptr;
+    int* dev_worklists_pending = nullptr;
 
-    bool work_to_do = true;
+
+    int worklists_pending = ((node_count + THREAD_COUNT - 1)/THREAD_COUNT);
     int threadsPerBlock = 128;
     int block_count = THREAD_COUNT/threadsPerBlock;    
-    int work_column_count = ((node_count + THREAD_COUNT - 1)/THREAD_COUNT) + 50;
+    int work_column_count = worklists_pending + EXTRA_WORKLISTS;
 
     int node_size = sizeof(BitVector) * source_count + sizeof(Node);
 
@@ -135,20 +139,20 @@ void multi_cuda::execute_analysis(Node* nodes, int node_count, Transfer* transfe
     dev_nodes = cuda_allocate_memory<Node>(node_size * node_count + 1);
     cuda_copy_to_device(dev_nodes, nodes, node_size * node_count);
 
-    dev_work_to_do = (bool*) (dev_nodes + node_count);
+    dev_worklists_pending = (int*) (dev_nodes + node_count);
 
     // Allocate transfer function
     dev_transfers = cuda_allocate_memory<Transfer>(sizeof(Transfer)*transfer_count);
     cuda_copy_to_device(dev_transfers, transfers, sizeof(Transfer)*transfer_count);
   
     Stopwatch lfp_watch;
-    int i = 0;
-    while(work_to_do){
-        work_to_do = false;
-        cuda_copy_to_device(dev_work_to_do, &work_to_do, 1);
+    int current_worklist = 0;
+    while(worklists_pending > 0){
+        --worklists_pending;
+        cuda_copy_to_device(dev_worklists_pending, &worklists_pending, sizeof(int));
 
         // Launch a kernel on the GPU with one thread for each element.
-        analyze<<<block_count, threadsPerBlock>>>(dev_nodes, (int(*)[THREAD_COUNT])dev_worklists, work_column_count+1, dev_transfers, node_count, dev_work_to_do, i, source_count);
+        analyze<<<block_count, threadsPerBlock>>>(dev_nodes, (int(*)[THREAD_COUNT])dev_worklists, work_column_count+1, dev_transfers, node_count, dev_worklists_pending, current_worklist, source_count);
         
         // Check for any errors launching the kernel
         cudaStatus = cudaGetLastError();
@@ -163,8 +167,8 @@ void multi_cuda::execute_analysis(Node* nodes, int node_count, Transfer* transfe
             fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
         }
         
-        cuda_copy_to_host((void*)&work_to_do, dev_work_to_do, sizeof(bool));
-        i = (i+1) % (work_column_count+1);
+        cuda_copy_to_host((void*)&worklists_pending, dev_worklists_pending, sizeof(int));
+        current_worklist = (current_worklist+1) % work_column_count;
     }
     lfp_watch.print_time<Microseconds>("LFP time: ");
 
