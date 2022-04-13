181f89c03180af73a3d0c2318f25472e6aa94d4d
Nicklas Hugoy <nicklas.hug@gmail.com>
2022-04-13 13:44:36 +0200

    add benchmark scripts for different analysis

---
+++ low-parallel-run_analysis.sh
@@ -0,0 +1,41 @@
+for i in {1..10}
+do 
+    ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_1.sc
+done
+
+for i in {1..10}
+do 
+    ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_5.sc
+done
+
+for j in {10..100..10}
+do
+    for i in {1..10}
+    do 
+        ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_$j.sc
+    done
+done
+
+for j in {150..500..50}
+do
+    for i in {1..10}
+    do 
+        ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_$j.sc
+    done
+done
+
+for j in {600..1500..100}
+do
+    for i in {1..10}
+    do 
+        ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_$j.sc
+    done
+done
+
+for j in {2000..10000..1000}
+do
+    for i in {1..10}
+    do 
+        ./build-release/analyzer -ba sampleprograms/low-parallel/low-parallel_$j.sc
+    done
+done
